c0fb000b798249e6e590fd0e93c798ea6090f745
Marvey Gold <145897274+MarveyGold@users.noreply.github.com>
2025-05-14 21:18:30 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+      <Script src="https://ligheechoagool.com/88/tag.min.js" data-zone="147503" async data-cfasync="false" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
         <footer>
