fcd52a2ee657d5f6c91bd239fe46516ca70d2406
Marvey <osoidaghemarvel@gmail.com>
2025-05-26 19:38:26 +0100

    ads

--- app/page.js
+++ app/page.js
@@ -1,33 +1,18 @@
-'use client'
-
-import { useEffect } from 'react'
 import Button from "./components/buttons"
 import styles from "./styles/home.module.css"
 import Logo from "./components/logo"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
 
 export default function Home() {
-  const pathname = usePathname()
-
-  useEffect(() => {
-    // Only load on homepage
-    if (pathname !== '/') return
-
-    const script = document.createElement('script')
-    script.src = "https://fpyf8.com/88/tag.min.js"
-    script.setAttribute("data-zone", "147503")
-    script.async = true
-    script.setAttribute("data-cfasync", "false")
-    document.body.appendChild(script)
-
-    return () => {
-      script.remove()
-    }
-  }, [pathname])
 
   return (
     <div className={styles.hom}>
+      <Script
+        src="https://fpyf8.com/88/tag.min.js"
+        data-zone="147503"
+        data-cfasync="false"
+        strategy="afterInteractive"
+      />
       <header>
         <Logo />
         <Link href="https://wa.me/+2349164747109">
