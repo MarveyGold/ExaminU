4a4bace05d0f459c99c80919afde18fa9d3f48ce
Marvey <osoidaghemarvel@gmail.com>
2025-08-30 13:02:26 +0100

    version 1.1.2.86

--- app/[faculty]/page.jsx
+++ app/[faculty]/page.jsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 export default async function Home({params}) {
     const {faculty} = await params;
     return(
         <div>
             <h3>{faculty} Not Available <b>Yet</b></h3>
+            <footer>
+                <Link href={`/`}><button className="footerButton">Go home</button></Link>
+            </footer>
         </div>
     )
 }
--- app/b/page.jsx
+++ app/b/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from "next/link";
+import Logo from "../components/logo";
 
 export default function Home() {
     const facultyList = ['PSC', 'LSC', 'BMS', 'ENG', 'AGR', 'DEN', 'EDU', 'ENV', 'MED', 'MGS', 'PHA', 'SPESSE', 'VNM'];
@@ -8,8 +9,9 @@ export default function Home() {
 
     return (
         <>
-            <h1>Select Your Faculty</h1>
-            <div>
+            <Logo />
+            <main>
+                <h2>Select Your Faculty</h2>
                 {facultyList.map((item, index) => (
                    <Link href={item} key={index}> <button 
                     key={index}
@@ -20,7 +22,7 @@ export default function Home() {
                     </Link>
                 )
                 )}
-            </div>
+            </main>
             <footer>
                             <Link href={`/`}><button className="footerButton"><h5>Change Mode</h5></button></Link>
                         </footer>
