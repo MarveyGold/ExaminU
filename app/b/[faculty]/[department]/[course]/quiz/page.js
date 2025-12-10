e83a08baf5b6b10c5ab68a89732919604ffe5d97
Marvey <osoidaghemarvel@gmail.com>
2025-12-10 16:27:48 +0100

    added option e

--- app/b/[faculty]/[department]/[course]/quiz/page.js
+++ app/b/[faculty]/[department]/[course]/quiz/page.js
@@ -4,122 +4,131 @@ import Logo from "../../../../../components/logo";
 import styles from "@/app/styles/quiz.module.css"
 import { redirect } from "next/navigation";
 import Form from "next/form";
-export async function generateMetadata({params}) {
- const {faculty, department, course} = await params;
+export async function generateMetadata({ params }) {
+  const { faculty, department, course } = await params;
 
   return {
-title: `${course.toUpperCase()} Quiz`,
-  description: `Test Yourself with random ${course.toUpperCase()} past questions and likely exam questions`,
+    title: `${course.toUpperCase()} Quiz`,
+    description: `Test Yourself with random ${course.toUpperCase()} past questions and likely exam questions`,
   };
 }
 
-export default async function Quiz({params, searchParams}) {
-  const {faculty, department, course} = await params;
+export default async function Quiz({ params, searchParams }) {
+  const { faculty, department, course } = await params;
   const src = `${course}.json`
-    const filePath = process.cwd() + `/public/data/courses/${src}`;
-    const result = await searchParams?.result || "";
-    
+  const filePath = process.cwd() + `/public/data/courses/${src}`;
+  const result = await searchParams?.result || "";
 
-   
 
-    const back = `/b/${faculty}/${department} `;
-    const again = `${back}/${course}/quiz`;
 
-    
-    try {
+
+  const back = `/b/${faculty}/${department} `;
+  const again = `${back}/${course}/quiz`;
+
+
+  try {
     const file = await fs.readFile(filePath, 'utf8');
     const data = JSON.parse(file);
     const randomIndex = await searchParams?.index ? parseInt(searchParams.index, 10) : Math.floor(Math.random() * data.length);
     const quiz = data[randomIndex];
-     const checkAnswer = async (formData) => {
-        'use server';
-        const selectedAnswer =  formData.get('quiz');
-        const correctAnswer = quiz.correctAnswer;
-        const result = selectedAnswer === correctAnswer ? "CORRECT!!! the answer is:" + correctAnswer : "Wrong, the answer is:" + " " + correctAnswer;
-        if (selectedAnswer=== null) {
-            return "The answer is" + correctAnswer
-        }
-        redirect(`/b/${faculty}/${department}/${course}/quiz?result=${encodeURIComponent(result)}&index=${randomIndex}`);
-    
+    const checkAnswer = async (formData) => {
+      'use server';
+      const selectedAnswer = formData.get('quiz');
+      const correctAnswer = quiz.correctAnswer;
+      const result = selectedAnswer === correctAnswer ? "CORRECT!!! the answer is:" + correctAnswer : "Wrong, the answer is:" + " " + correctAnswer;
+      if (selectedAnswer === null) {
+        return "The answer is" + correctAnswer
+      }
+      redirect(`/b/${faculty}/${department}/${course}/quiz?result=${encodeURIComponent(result)}&index=${randomIndex}`);
+
 
     }
-     
-    return(
-       <main>
-      <div className={styles.home}>
-           
-            <div className={styles.title}>
-             {quiz.title}
-            </div>
-            <code>
-              {quiz.instruction}
-            </code>
 
-            <h4>
-              {quiz.question}
-           </h4>
+    return (
+      <main>
+        <div className={styles.home}>
 
-            <Form action={checkAnswer}>
+          <div className={styles.title}>
+            {quiz.title}
+          </div>
+          <code>
+            {quiz.instruction}
+          </code>
+
+          <h4>
+            {quiz.question}
+          </h4>
+
+          <Form action={checkAnswer}>
             <div>
-                <label className="selector">
-                  <input
-                    type="radio" 
-                    name="quiz" 
-                    value={quiz.optionA} 
-                  />
-                  {quiz.optionA}
-                </label>
-                
-                <label className="selector">
-                  <input
-                    type="radio" 
-                    name="quiz" 
-                    value={quiz.optionB} 
-                  />
-                  {quiz.optionB}
-                </label>
-                <label className="selector">
-                  <input
-                    type="radio" 
-                    name="quiz" 
-                    value={quiz.optionC} 
-                  />
-                  {quiz.optionC}
-                </label>
-                <label className="selector">
-                  <input
-                    type="radio" 
-                    name="quiz" 
-                    value={quiz.optionD} 
-                  />
-                  {quiz.optionD}
-                </label>
+              <label className="selector">
+                <input
+                  type="radio"
+                  name="quiz"
+                  value={quiz.optionA}
+                />
+                {quiz.optionA}
+              </label>
+
+              <label className="selector">
+                <input
+                  type="radio"
+                  name="quiz"
+                  value={quiz.optionB}
+                />
+                {quiz.optionB}
+              </label>
+              <label className="selector">
+                <input
+                  type="radio"
+                  name="quiz"
+                  value={quiz.optionC}
+                />
+                {quiz.optionC}
+              </label>
+              <label className="selector">
+                <input
+                  type="radio"
+                  name="quiz"
+                  value={quiz.optionD}
+                />
+                {quiz.optionD}
+              </label>
+              <label className="selector">
+                <input
+                  type="radio"
+                  name="quiz"
+                  value={quiz.optionE}
+                />
+                {quiz.optionE}
+              </label>
+
             </div>
             <div className="ans">
-                <button type="submit"  className="answerButton">Check The Answer</button>
+              <button type="submit" className="answerButton">Check The Answer</button>
             </div>
 
-            </Form>
-          {result && <h5>{result}</h5> }
-            
-            <div id="preFooter">
+          </Form>
+          {result && <h5>{result}</h5>}
 
-            </div>
-            <footer >
-                <Link href={back}><button className={styles.footerButton}><h5>Change Course</h5></button></Link>
-                <Link href={again}><button className={styles.footerButton}>Change Question</button></Link>
-            </footer>
+          <div id="preFooter">
 
-            
-    </div>
- </main>
-      
+          </div>
+          <footer >
+            <Link href={back}><button className={styles.footerButton}><h5>Change Course</h5></button></Link>
+            <Link href={again}><button className={styles.footerButton}>Change Question</button></Link>
+          </footer>
+
+
+        </div>
+      </main>
+
     )
-    } catch (error) {
-      return <h1>This course is not yet available</h1>
-    }
-    
-    // const quiz = data.find(quiz => quiz.id === random);
-    
-    }
-    
+  } catch (error) {
+    return <h1>This course is not yet available</h1>
+  }
+
+  // const quiz = data.find(quiz => quiz.id === random);
+
+}
+
