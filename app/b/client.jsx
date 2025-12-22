6f0924007286df40976e422d7e9530eed2fcc443
Marvey <osoidaghemarvel@gmail.com>
2025-12-22 12:55:16 +0100

    moved interactions to separate client component

---
+++ app/b/client.jsx
@@ -0,0 +1,43 @@
+"use client";
+import { useState } from "react";
+import styles from "../styles/button.module.css";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+export default function Faculty({ names, codes }) {
+  const alert = useSearchParams().get('alert');
+  const [current, setCurrent] = useState();
+  const [search, setSearch] = useState("");
+  const filtered = names
+    .map((f, i) => ({ name: f, abbr: codes[i] }))
+    .filter((f) => f.name.toLowerCase().includes(search.toLowerCase()));
+  return (
+    <div className={styles.container}>
+      <input
+        type='text'
+        placeholder="Search"
+        value={search}
+        onChange={(e) =>
+          setSearch(e.target.value)
+        }
+        className='selector'
+      />
+      <div>
+        {filtered.map((faculty) => (
+          <button
+            key={faculty.abbr}
+            onClick={() =>
+              setCurrent(faculty.abbr)
+            }
+            className={`course selector ${current === faculty.abbr ? 'selected' : ''}`}
+          >
+            {faculty.name}
+          </button>
+        ))}
+      </div>
+      <footer>
+        <Link href={`b/${current}/?selected=0`}>{current && <button className="footerButton">Get Started</button>}</Link>
+      </footer>
+    </div>
+  );
+}
