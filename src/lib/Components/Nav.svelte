7e74b68747366688bf8f4fcde78d4d00a3a4097a
Marvey Gold <145897274+MarveyGold@users.noreply.github.com>
2026-07-08 11:04:46 +0100

    Update Nav.svelte

--- src/lib/Components/Nav.svelte
+++ src/lib/Components/Nav.svelte
@@ -18,10 +18,11 @@
 <nav>
   <button onclick={changeStyle} class="nav-btn">{btnText}</button>
   <ul id="list" class={style}>
-    <li class="mobile"><Auth class="mobile" /></li>
-    <li><a href="/">Home</a></li>
+ <li><a href="/">Home</a></li>
     <li><a href="#practice">Curriculum</a></li>
     <li><a href="#contact">Feedback</a></li>
+    <li class="mobile"><Auth class="mobile" /></li>
+   
   </ul>
 </nav>
 
