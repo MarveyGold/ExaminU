5ccbb59cfaa14a8a49deee074037ce0f4c3770c9
Marvey <osoidaghemarvel@gmail.com>
2026-07-23 01:59:23 +0100

    updated db wt postutme

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -2,6 +2,6 @@ import { json } from "@sveltejs/kit";
 import Data from "$lib/server/models/data.js";
 
 export async function GET({ request, event }) {
-  const api = await Data.find()
+  const api = await Data.find({ exam: "post-utme" })
   return json(api);
 }
--- src/routes/asp/[subject]/+page.server.js
+++ src/routes/asp/[subject]/+page.server.js
@@ -6,7 +6,7 @@ export async function load({ params, fetch, url }) {
 
   const res = await fetch(`/data/subjects/${subject}.json`)
   const all = await res.json();
-  await Quiz.insertMany(all);
+  // await Quiz.insertMany(all);
   const questions = all.map(item => item.question);
   const q = url.searchParams.get('q') ?? questions[0];
   const randomIndex = Math.floor(Math.random() * questions.length);
