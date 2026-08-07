6b259456eeee07e5fea3d8f728039f682f989554
Marvey Gold <145897274+MarveyGold@users.noreply.github.com>
2026-08-07 10:38:14 +0100

    Simplify GET request for quiz data
    Refactored GET function to simplify query and remove unnecessary update operation.

--- src/routes/api/quiz/+server.js
+++ src/routes/api/quiz/+server.js
@@ -2,23 +2,6 @@ import { json } from "@sveltejs/kit";
 import Quiz from "$lib/server/models/quiz.js";
 
 export async function GET({ request, event }) {
-  await Quiz.updateMany(
-    { exam: "school", optionA: { $exists: true } },
-    [
-      {
-        $set: {
-          options: {
-            $filter: {
-              input: ["$optionA", "$optionB", "$optionC", "$optionD", "$optionE"],
-              as: "opt",
-              cond: { $ne: ["$$opt", null] }
-            }
-          }
-        }
-      },
-      { $unset: ["optionA", "optionB", "optionC", "optionD", "optionE"] }
-    ],
-    { updatePipeline: true }
-  ); const api = await Quiz.find();
+ const api = await Quiz.find({exam: "school"});
   return json(api);
 }
